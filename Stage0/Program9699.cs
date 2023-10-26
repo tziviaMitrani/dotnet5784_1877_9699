4efe2cf87ee2f1b35ee394a43d53933c2cec2df4
tzivia-mitrani <tzivia2004@gmail.com>
2023-10-26 15:26:14 +0300

    first

---
+++ Stage0/Program9699.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stage0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9699();
+            Console.ReadKey();
+        }
+
+        private static void Welcome9699()
+        {
+            Console.WriteLine("Enter your nam: ");
+            string user = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first consol application", user);
+        }
+    }
+}
